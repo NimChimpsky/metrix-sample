6f031e2abb5446dbaaf176f993e5806798ff5a68
Stephen Batty <sbatty@gmail.com>
2019-09-13 15:45:46 +1000

    abs zero

--- src/main/java/au/com/metricsoftware/sample/SampleApp.java
+++ src/main/java/au/com/metricsoftware/sample/SampleApp.java
@@ -3,11 +3,13 @@ package au.com.metricsoftware.sample;
 import au.com.metricsoftware.metrix.MetrixServer;
 
 public class SampleApp {
+    private static final String[] controllerPackages
+            = {"au.com.metricsoftware.sample.controller", "au.com.metricsoftware.sample.other_controllers"};
 
     public static void main(String[] quartet) throws Exception {
         MetrixServer metrixServer = new MetrixServer.Builder().withPort(8080)
-                                                              .withApiContext("sample-api")
-                                                              .withControllerPackages("au.com.metricsoftware.sample.controller", "au.com.metricsoftware.sample.other_controllers")
+                                                              .withApiContext("/sample-api")
+                                                              .withControllerPackages(controllerPackages)
                                                               .build();
         metrixServer.start();
     }
